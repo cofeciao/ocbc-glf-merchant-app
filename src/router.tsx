e2c43f66aca5ad5ef0b77d9eeae358df5c8d20a7
Minh Tran <minh.tran@techvsi.com>
2023-02-20 11:36:50 +0700

    Feat: additional path self-serve into router

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import GlobalContext from "./common/GlobalContext";
-import Home from "./views/landing";
+import Home from "./views/home";
 import DemoForm from "./views/demo-form";
 
 export default [
   <Route exact path="/" component={Home} key="/" />,
-  <Route exact path="/demo-form" component={DemoForm} key="/demo-form" />,
+  <Route exact path="/self-serve" component={DemoForm} key="/self-serve" />,
 ];
