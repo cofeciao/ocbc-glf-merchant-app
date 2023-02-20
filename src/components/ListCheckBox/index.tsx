31a3faa1c8a3de4c5eccd85a6681b0ecab99b909
Minh Tran <minh.tran@techvsi.com>
2023-02-20 11:02:39 +0700

    Feat: add ListCheckBox component

--- src/components/ListCheckBox/index.tsx
+++ src/components/ListCheckBox/index.tsx
@@ -6,7 +6,7 @@ import {
   Checkbox,
   FormGroup,
   Grid,
-  Item,
+  // Item,
 } from "@material-ui/core";
 import { CardCheckbox, Category } from "@sectionsg/orc";
 import classnames from "classnames/bind";
@@ -20,52 +20,74 @@ import { IListCheckBox, ICheckBox } from "./ListCheckBox";
 
 // render UI
 const ListCheckbox = (props: IListCheckBox) => {
-  const { label, textError, dataCardCheckbox, checkboxKey, getValue } = props;
-  const [dataCard, setDataCard] = useState<ICheckBox[]>(dataCardCheckbox);
+  const {
+    label,
+    textError,
+    dataCardCheckbox,
+    checkboxKey,
+    getValue,
+    xs,
+    sm,
+    md,
+    lg,
+  } = props;
+  // const [dataCard, setDataCard] = useState<ICheckBox[]>(dataCardCheckbox);
   const cx = classnames.bind(styles);
-  useEffect(() => {
-    setDataCard(dataCard);
-    getValue(dataCard);
-  }, [dataCard]);
+
+  /**
+   * Listening for changes from data to update state
+   */
+  // useEffect(() => {
+  //   setDataCard(dataCard);
+  // }, [dataCardCheckbox]);
+
+  /**
+   * Run after clicking any item checkbox to process data stream
+   * @param event
+   * @param checked
+   */
+  const handleCheckBox = (event: any, checked: boolean) => {
+    const newData: any = dataCardCheckbox.reduce((acc, item) => {
+      const newItem: any = { ...item };
+      newItem.checked = false;
+      if (newItem.value === event.target.value) {
+        newItem.checked = checked;
+      }
+      acc.push(newItem);
+      return acc;
+    }, []);
+    newData.name = event.target.value;
+    getValue(newData);
+  };
 
   return (
     <FormGroup className={cx("list-checkbox-wrapper")}>
-      {dataCard.map((item: any) => {
+      {dataCardCheckbox.map((item: any, index: number) => {
         return (
-          <Grid item xs={8}>
-            {/* <Item> */}
-              <FormControlLabel
-                key={checkboxKey}
-                label={
-                  <div className={cx("card-checkbox-content")}>
-                    {item.label && (
-                      <span className={cx("card-checkbox-content--title")}>
-                        {item.label}
-                      </span>
-                    )}
-                    {item.description && (
-                      <p className={cx("card-checkbox-content--description")}>
-                        {item.description}
-                      </p>
-                    )}
-                  </div>
-                }
-                onChange={(event: any, checked: boolean) => {
-                  const newData: any = dataCard.map((item: any) => {
-                    item.checked = false;
-                    if (item.value === event.target.value) {
-                      item.checked = checked;
-                    }
-                    return item;
-                  });
-                  newData.name = event.target.value;
-                  setDataCard(newData);
-                }}
-                checked={item.checked}
-                value={item.value}
-                control={<Checkbox />}
-              />
-            {/* </Item> */}
+          <Grid key={index} item xs={xs} sm={sm} md={md} lg={lg}>
+            {/* {Form Control containing check boxes inside} */}
+            <FormControlLabel
+              label={
+                <div className={cx("item-checkbox-content")}>
+                  {item.label && (
+                    <span className={cx("item-checkbox-content--title")}>
+                      {item.label}
+                    </span>
+                  )}
+                  {item.description && (
+                    <p className={cx("item-checkbox-content--description")}>
+                      {item.description}
+                    </p>
+                  )}
+                </div>
+              }
+              onChange={(event: any, checked: boolean) =>
+                handleCheckBox(event, checked)
+              }
+              checked={item.checked}
+              value={item.value}
+              control={<Checkbox />}
+            />
           </Grid>
         );
       })}
