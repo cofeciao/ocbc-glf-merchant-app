c2f139c967d0b21b8e64bd21b5ac469a763bd81f
Keei Yuan <89989087+s4keeiyuan@users.noreply.github.com>
2023-02-23 12:16:37 +0800

    another try

--- config/staging.js
+++ config/staging.js
@@ -5,7 +5,7 @@ module.exports = {
   env: 'staging',
   myinfo: {
     api: 'https://test.api.myinfo.gov.sg/com/v3/authorise',
-    redirectUri: 'https://home-loan-services.ocbc-mfe.wearesection.com',
+    redirectUri: 'https://ocbc.ext1.wearesection.com/glf-merchant-app-staging',
     clientIdRepricing: 'STG-193200032W-OCBC-MORTGAGEREPRICE',
     secretRepricing: 'kbMksFgNFJFo7eHIVQqi4cX15wiVsjSg',
     clientIdNonRepricing: 'STG-193200032W-OCBC-MORTGAGEPREPAYMENT',
