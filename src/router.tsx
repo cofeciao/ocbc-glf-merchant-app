60ff749e4edb1524ff8a08c152f6dc22e21017fe
thi-bt <thi.bui@mlpert.com>
2023-02-26 18:38:13 +0700

    reat: build UI for launch confirmtion

--- src/router.tsx
+++ src/router.tsx
@@ -5,9 +5,12 @@ import GlobalContext from "./common/GlobalContext";
 import Home from "./views/landing";
 import DemoForm from "./views/demo-form";
 import LoginPage from "./views/login";
+import LaunchConfirmtion from "./views/launch-confirmtion"
 
 export default [
   <Route exact path="/" component={Home} key="/" />,
   <Route exact path="/login" component={LoginPage} key="/login" />,
+  <Route exact path="/launch-confirmtion" component={LaunchConfirmtion} key="/launch-confirmtion" />,
+  
   <Route exact path="/demo-form" component={DemoForm} key="/demo-form" />,
 ];
---
+++ src/views/launch-confirmtion/index.tsx
@@ -0,0 +1,64 @@
+import classnames from "classnames/bind";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import { Container, Grid } from "@material-ui/core";
+
+// import style
+import styles from "./LaunchConfirmtion.scss";
+
+// import component lib
+import {
+  Button,
+  Link,Header,Footer
+} from '@sectionsg/orc';
+import { useSelector } from "react-redux";
+
+//import contants
+import {TITLE_PAGE,LINK_EXTERNAL_PAGE} from "../../utils/constants";
+
+const LaunchConfirmtionPage: React.FC = ( { }) => {
+  const cx = classnames.bind(styles);
+  const history = useHistory();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+
+  return (
+    <>
+      <div className={cx('launch-confirmtion-page')}>
+        <Header
+            namePage={TITLE_PAGE}
+            backLink={{ name: TITLE_PAGE, href: LINK_EXTERNAL_PAGE }}
+          />
+        <Container className={cx('container')}>
+            <Grid container>
+              <Grid item xs></Grid>
+              <Grid item lg={7} xs={7} md={12} sm={12}>
+                <div className={cx('launch-confirmtion-content', 'container', 'mt-mb-30')}>
+                  <div className={"title-wrapper"}>
+                    <div className={cx("title-text")}><span>You will launch the company’s application form</span></div>
+                  </div>
+                  <div className='description'>
+                    <p className={'mb-dt-40'}>You may use the next pages onwards to gather information from the company.</p>
+                  </div>
+                  
+                  {/* Section button  */}
+                  <section className={cx('button-wrapper', 'd-flex mt-dt-40')}>
+                    <div className="d-inline group-submit">
+                      <Button backgroundClass="bgGunmetalBluegrey" onClick={() => history.push('/')}>Yes, launch the form</Button>
+                      <Link to='/'>Back</Link>
+                    </div>
+                  </section>
+                </div>
+              </Grid>
+              <Grid item xs></Grid>
+            </Grid>
+        </Container>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default LaunchConfirmtionPage;
