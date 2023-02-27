d35d958babed2fea3806b64765d69fa92f600239
Minh Tran <minh.tran@techvsi.com>
2023-02-27 22:38:14 +0700

    Feat: additional self-serve into router

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Route } from "react-router-dom";
 import GlobalContext from "./common/GlobalContext";
 import Home from "./views/home";
-import DemoForm from "./views/demo-form";
+import SelfServe from "./views/self-serve";
 
 export default [
   <Route exact path="/" component={Home} key="/" />,
-  <Route exact path="/self-serve" component={DemoForm} key="/self-serve" />,
+  <Route exact path="/self-serve" component={SelfServe} key="/self-serve" />,
+  <Route exact path="/self-serve/:slug" component={SelfServe} key="/self-serve/:slug" />,
 ];
