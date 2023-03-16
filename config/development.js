77106ab345d563b486df6f0c910da48b215ca6fb
Minh Tran <minh.tran@techvsi.com>
2023-03-16 11:54:04 +0700

    Feat: update redirectUri for development env

--- config/development.js
+++ config/development.js
@@ -5,7 +5,7 @@ module.exports = {
     env: 'development',
     myinfo: {
       api: 'https://test.api.myinfo.gov.sg/com/v3/authorise',
-      redirectUri: 'https://home-loan-services.ocbc-mfe.wearesection.com',
+      redirectUri: 'http://localhost:9000/',
       clientIdRepricing: 'STG-193200032W-OCBC-MORTGAGEREPRICE',
       secretRepricing: 'kbMksFgNFJFo7eHIVQqi4cX15wiVsjSg',
       clientIdNonRepricing: 'STG-193200032W-OCBC-MORTGAGEPREPAYMENT',
