c3c4f31532de0653a644c89f0ca6396050289ce7
Minh Tran <minh.tran@techvsi.com>
2023-05-06 21:26:33 +0700

    Feat: remove pdf

--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,6 @@ import { Route } from "react-router-dom";
 import HomePage from "./views/home";
 import SelfServe from "./views/self-serve";
 import AcknowledgementPage from "./views/acknowledgement";
-import MyPDF from "./views/self-serve/pdfviewer/index";
 
 export default [
   // Home
@@ -19,7 +18,4 @@ export default [
     component={AcknowledgementPage}
     key="/acknowledgement/:slug"
   />,
-
-  // Pdf Viewer
-  <Route exact path="/pdf" component={MyPDF} key="/pdf" />,
 ];
